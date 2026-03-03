5242737b5df103b2a09c1802a1db649c76e1f05f
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-03 16:40:50 -0300

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,31 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+test('normalizeURL', () => { // Name of the test, function
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected) // I'm expecting the actual output of the normalized URL to be equal to the expected I specified
+    // If it's equal, it passed, if not, the test failed
+})
+
+test('normalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip different protocols', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
