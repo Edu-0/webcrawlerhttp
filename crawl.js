50663fc5ae27ff76cefad411247f8556329c0460
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-03 17:43:57 -0300

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){ // The utility for that is to grab all the clickable links inside a HTML web page.
+    const urls = []
+    const dom = new JSDOM(htmlBody) // Taking that string and creating a document object model
+    const linkElements = dom.window.document.querySelectorAll('a') // Gives a list of all "<a>" tags
+    for (const linkElement of linkElements){ // .href corresponds to the link text
+        if (linkElement.href.slice(0,1) === "/"){
+            // Relative URL
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.log(`Error with relative URL: ${err.message}`)
+            }
+        } else{
+            // Absolute URL
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err){
+                console.log(`Error with absolute URL: ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -7,6 +35,4 @@ function normalizeURL(urlString){
     return hostPath;
 }
 
-module.exports = {
-    normalizeURL
-}
+module.exports = { normalizeURL, getURLsFromHTML }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL', () => { // Name of the test, function
@@ -27,5 +27,80 @@ test('normalizeURL strip different protocols', () => {
     const input = 'http://blog.boot.dev/path/'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseUrl = "https://blog.boot.dev/path/"
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => { // Here we want to extract the URL from the HTML body, but it's a relative URL, so we need to combine it with the base URL to get the full URL.
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseUrl = "https://blog.boot.dev"
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+    const inputHTMLBody = `
+    <html>
+    <body>
+            <a href="https://blog.boot.dev/path1/">
+                Boot.dev Blog Path 1
+            </a>
+            <a href="/path2/">
+                Boot.dev Blog Path 2
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseUrl = "https://blog.boot.dev"
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="invalid">
+                Invalid URL
+            </a>
+        </body>
+    </html>
+    `
+
+    const inputBaseUrl = "https://blog.boot.dev"
+
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+    const expected = []
     expect(actual).toEqual(expected)
 })
