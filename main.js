15ee4319e54499e00425cf8390b2dbe29846cdf6
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-03-04 15:57:53 -0300

    printReport function finished

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // This script will take as input a website and crawl it.
 
 const {crawlPage} = require('./crawl.js')
+const {printReport} = require('./report.js')
 
 async function main(){
     if (process.argv.length < 3){
@@ -17,10 +18,8 @@ async function main(){
 
     console.log(`Starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
-
-    for (const page of Object.entries(pages)){
-        console.log(page)
-    }
+    
+    printReport(pages)
 }
 
 main()
